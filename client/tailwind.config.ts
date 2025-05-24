4c379c52397ad447b0b312198fb6861da624fdef
Kareem <kareemalmond@gmail.com>
2025-05-24 02:57:18 -0700

    added new section

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -97,9 +97,6 @@ const config = {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
-            transitionDuration: {
-                "5000": "5000ms",
-            },
         },
     },
     plugins: [
