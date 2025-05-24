ac04b55a0e7461746d8534dba1033b1506cb8ee0
Kareem <kareemalmond@gmail.com>
2025-05-24 03:38:36 -0700

    TRNASFERS WPORKSASDASDASDASDASAD

--- client/src/components/live/TranscriptPanel.tsx
+++ client/src/components/live/TranscriptPanel.tsx
@@ -1,21 +1,14 @@
 import { useEffect, useState } from "react";
 import { CallProps } from "@/app/live/page";
-import { CheckCircle2Icon, HeadsetIcon, PhoneIcon } from "lucide-react";
+import { CheckCircle2Icon } from "lucide-react";
 
-import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import ChatInterface from "./ChatInterface";
 import EmotionCard from "./EmotionCard";
 
-interface TranscriptPanelProps extends CallProps {
-    handleTransfer: (id: string) => void;
-}
+interface TranscriptPanelProps extends CallProps {}
 
-const TranscriptPanel = ({
-    call,
-    selectedId,
-    handleTransfer,
-}: TranscriptPanelProps) => {
+const TranscriptPanel = ({ call, selectedId }: TranscriptPanelProps) => {
     const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
@@ -31,12 +24,10 @@ const TranscriptPanel = ({
         a.intensity < b.intensity ? 1 : -1,
     );
 
-    if (!call) {
-        return null;
-    }
-
     return (
-        <div className={`w-[400px] bg-white`}>
+        <div
+            className={`w-[400px] bg-white transition-transform duration-1000 ease-in-out ${isVisible ? "translate-x-0" : "translate-x-full"} ${selectedId ? "visible" : "hidden"}`}
+        >
             <p className="px-2 py-[6px]">Live Transcript</p>
             <Separator />
 
@@ -69,13 +60,6 @@ const TranscriptPanel = ({
                     </div>
 
                     <ChatInterface call={call} selectedId={selectedId} />
-
-                    <Button
-                        onClick={() => handleTransfer(call.id)}
-                        className="w-full space-x-2 bg-green-500 hover:bg-green-500/80"
-                    >
-                        <HeadsetIcon /> <p>Transfer</p>
-                    </Button>
                 </div>
             </div>
         </div>
