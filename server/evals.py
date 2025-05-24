02c149d5f30759908301f25897a5dc049faa1382
Kareem <kareemalmond@gmail.com>
2025-05-24 06:10:02 -0700

    Update requirements and add inference notebook

--- server/evals.py
+++ server/evals.py
@@ -48,18 +48,9 @@ async def eval(message: str, current_data: str):
     )
     return evals.choices[0].message.content
 
-
 interesting_emotions = [
-    "anger",
-    "anxiety",
-    "calmness",
-    "concentration",
-    "confusion",
-    "distress",
-    "fear",
-    "horror",
-    "pain",
-    "sadness",
+    "anger", "anxiety", "calmness", "concentration", "confusion",
+    "distress", "fear", "horror", "pain", "sadness"
 ]
 
 
@@ -79,7 +70,7 @@ async def hume_eval(message: str):
         for word_data in emotions_data:
             word_emotions = word_data.get("emotions", [])
             for emotion in word_emotions:
-                if emotion["name"].lower() in interesting_emotions:
+                if emotion["name"] in interesting_emotions:
                     if emotion["name"] in emotion_scores:
                         emotion_scores[emotion["name"]] += emotion["score"]
                     else:
